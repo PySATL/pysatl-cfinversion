5e2005cd7650fc93782761b445e3c1ab4cc6ed8d
Lily <lily@MacBook-Air-Lily.local>
2025-02-21 20:11:10 +0300

    fix bug in normal distribution

--- CFInvert/Distributions/normal.py
+++ CFInvert/Distributions/normal.py
@@ -5,15 +5,15 @@ from scipy.stats import norm
 from CFInvert.Distributions.AbstractDistribution import AbstractDistribution
 
 class Norm(AbstractDistribution):
-    def __init__(self, m: float, var: float) -> None:
+    def __init__(self, m: float, sd: float) -> None:
         """
         Конструктор класса Norm.
 
         :param m: математическое ожидание (среднее значение) нормального распределения
-        :param var: дисперсия нормального распределения
+        :param sd: стандартное отклонение
         """
         self.m = m
-        self.var = var
+        self.sd = sd
 
     def chr(self,  x: np.ndarray) -> np.ndarray:
         """
@@ -22,7 +22,7 @@ class Norm(AbstractDistribution):
         :param x: аргумент характеристической функции
         :return: значение характеристической функции в точке x
         """
-        return exp(self.m * 1j * x - (self.var * (x ** 2)) / 2)
+        return exp(self.m * 1j * x - ((self.sd * x) ** 2 / 2))
 
     def cdf(self,  x: np.ndarray) -> np.ndarray:
         """
@@ -31,7 +31,7 @@ class Norm(AbstractDistribution):
         :param x: аргумент функции распределения
         :return: значение функции распределения в точке x
         """
-        return norm.cdf(x, loc=self.m, scale=self.var)
+        return norm.cdf(x, loc=self.m, scale=self.sd)
 
     def pdf(self,  x: np.ndarray) -> np.ndarray:
         """
@@ -40,4 +40,4 @@ class Norm(AbstractDistribution):
         :param x: аргумент плотности вероятности
         :return: значение плотности вероятности в точке x
         """
-        return norm.pdf(x, loc=self.m, scale=self.var)
+        return norm.pdf(x, loc=self.m, scale=self.sd)
