8922a2f42dd3fd448d01c14f3419f6b65b868983
desiment <desiment@yandex.ru>
2025-06-07 23:01:18 +0300

    fix: final fixes

--- cfinversion/tools/validation_tools.py
+++ cfinversion/tools/validation_tools.py
@@ -11,8 +11,11 @@ def lre(v_true: np.ndarray, v: np.ndarray) -> np.ndarray:
     :param v: predicted value
     :return: log relative error
     """
-    rel_errors = np.abs((v_true[v_true != 0] - v[v_true != 0]) / v_true[v_true != 0])
-    return -np.log10(rel_errors[rel_errors != 0])
+    rel_errors, nlre = np.zeros_like(v_true), np.zeros_like(v_true)
+    rel_errors[v_true != 0] = np.abs((v_true[v_true != 0] - v[v_true != 0]) / v_true[v_true != 0])
+    nlre[rel_errors != 0] = -np.log10(rel_errors[rel_errors != 0])
+    nlre[rel_errors == 0] = np.nan
+    return nlre
 
 def l0_err(f: Callable, tol_diff:float = 1e-3) -> float:
     return 1 - sp.integrate.quad(f, -np.inf, np.inf, epsabs = tol_diff)[0]
